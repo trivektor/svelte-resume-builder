5810efd595eb0b53eb0554fbfd1bc2aed7df64ad
Tri Vuong <trivektor@gmail.com>
2020-07-26 03:46:02 -0700

    Support section deletion

--- src/components/ResumeForm.svelte
+++ src/components/ResumeForm.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Sections from './Sections.svelte';
+  import Section from './Section.svelte';
   import {resumeStore} from '../stores';
 
   let resume = {};
@@ -17,20 +17,34 @@
       ],
     }));
   }
+
+  function deleteSection(event) {
+    const {detail: {sectionIndex}} = event;
+    const updatedSections = (resume.sections || []).concat([]);
+
+    updatedSections.splice(sectionIndex, 1);
+
+    resumeStore.update((state) => ({
+      ...state,
+      sections: updatedSections,
+    }));
+  }
 </script>
 
 <h2 class="mb-2">Overview</h2>
 <div class="mb-4">
   <label class="block text-gray-700 font-bold">Name</label>
-  <input class="shadow w-full px-1 py-1" bind:value={resume.name} />
+  <input class="w-full px-2 py-2" bind:value={resume.name} />
 </div>
 <div class="mb-4">
   <label class="block text-gray-700 font-bold">Description</label>
-  <textarea class="block w-full" bind:value={resume.description}></textarea>
+  <textarea class="block w-full px-2 py-2" bind:value={resume.description}></textarea>
 </div>
 {#if resume.id}
   <h2 class="mt-2 mb-2">Sections</h2>
-  <Sections sections={resume.sections} />
+  {#each resume.sections as section, sectionIndex}
+    <Section section={section} {sectionIndex} on:delete={deleteSection} />
+  {/each}
 {/if}
 <div>
   {#if resume.id}
